9d66a36499f6b3c8ecb433e51fb7049e65ae4491
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-03-28 22:14:39 +0700

    fix responsive

--- pages/login/index.js
+++ pages/login/index.js
@@ -6,24 +6,24 @@ export default function Login() {
 	const img =
 		'https://images.unsplash.com/photo-1593537898540-b8b821014c8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80';
 	return (
-		<div className="grid grid-cols-3">
+		<div className="flex flex-col md:grid md:grid-cols-3">
 			<div>
 				<div
-					className="flex items-center justify-center h-screen w-full"
+					className="flex items-center justify-center h-[150px] md:h-screen w-full"
 					style={{
 						background: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${img})`,
 						backgroundSize: 'cover',
 					}}
 				>
 					<div className="flex items-center">
-						<Icon icon="ic:round-mode-of-travel" width={80} color="#fff" />
-						<p className="font-bold text-5xl text-white ml-1 cursor-default">
+						<Icon icon="ic:round-mode-of-travel" width={70} color="#fff" />
+						<p className="font-bold text-4xl text-white ml-1 cursor-default">
 							GoTour
 						</p>
 					</div>
 				</div>
 			</div>
-			<div className="text-center m-auto col-span-2 w-1/2">
+			<div className="text-center mt-48 md:mt-auto m-auto md:col-span-2 w-3/4 md:w-1/2">
 				<h1 className="font-bold text-4xl mb-10">Login</h1>
 				<Formik
 					initialValues={{ email: '', password: '' }}
