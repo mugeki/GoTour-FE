219ec8ecbe5bf027748ace1d70d11ce3d25c8a39
Faishal Irawan <muhammadfaishal193@gmail.com>
2022-04-13 11:19:04 +0700

    Fix load more prompt condition

--- pages/index.js
+++ pages/index.js
@@ -50,60 +50,59 @@ export default function Home() {
 			{data.length !== 0 && (
 				<>
 					<div className="absolute top-0 -z-10 bg-black/70 min-w-full min-h-screen"></div>
-						<div
-							className="absolute top-0 -z-20 min-w-full min-h-screen bg-cover transition-all duration-300"
-							style={{ backgroundImage: `url(${data[focusedItem].img_urls[0]})` }}
-						></div>
-						<main className="flex flex-col-reverse lg:flex-row justify-between items-center self-center lg:pt-28 px-10 lg:px-20 xl:px-36 text-white">
-							<div className="w-full md:w-3/4 lg:w-2/4 md:mr-10 mt-6 lg:mt-0">
-								<h1 className="font-bold text-4xl md:text-6xl mb-3 truncate">
-									{data[focusedItem].name}
-								</h1>
-								<p className="font-thin text-sm line-clamp-2 sm:line-clamp-5">
-									{data[focusedItem].description}
-								</p>
-								<Link href="/" passHref>
-									<Button
-										className="my-5"
-										type="submit"
-										size="lg"
-										variant="filled"
-										rightIcon={<Icon icon="eva:arrow-right-fill" />}
-									>
-										Explore
-									</Button>
-								</Link>
-							</div>
-							<Swiper
-								centeredSlides={true}
-								slidesPerView={2}
-								spaceBetween={10}
-								grabCursor={true}
-								autoplay={{
-									delay: 5000,
-									disableOnInteraction: false,
-								}}
-								onSlideChange={(swiper) => setFocusedItem(swiper.activeIndex)}
-								modules={[Autoplay, Pagination]}
-								className="w-full sm:w-2/4"
-							>
-								{data.map((item, i) => (
-									<SwiperSlide key={i} className="self-center">
-										<CardHome
-											id={item.id}
-											name={item.name}
-											rating={item.rating}
-											img_urls={item.img_urls[0]}
-											focused={i === focusedItem}
-											isWishlishted={item.wishlist}
-										/>
-									</SwiperSlide>
-								))}
-							</Swiper>
-						</main>
-					</>
-				)
-			}
+					<div
+						className="absolute top-0 -z-20 min-w-full min-h-screen bg-cover transition-all duration-300"
+						style={{ backgroundImage: `url(${data[focusedItem].img_urls[0]})` }}
+					></div>
+					<main className="flex flex-col-reverse lg:flex-row justify-between items-center self-center lg:pt-28 px-10 lg:px-20 xl:px-36 text-white">
+						<div className="w-full md:w-3/4 lg:w-2/4 md:mr-10 mt-6 lg:mt-0">
+							<h1 className="font-bold text-4xl md:text-6xl mb-3 truncate">
+								{data[focusedItem].name}
+							</h1>
+							<p className="font-thin text-sm line-clamp-2 sm:line-clamp-5">
+								{data[focusedItem].description}
+							</p>
+							<Link href="/" passHref>
+								<Button
+									className="my-5"
+									type="submit"
+									size="lg"
+									variant="filled"
+									rightIcon={<Icon icon="eva:arrow-right-fill" />}
+								>
+									Explore
+								</Button>
+							</Link>
+						</div>
+						<Swiper
+							centeredSlides={true}
+							slidesPerView={2}
+							spaceBetween={10}
+							grabCursor={true}
+							autoplay={{
+								delay: 5000,
+								disableOnInteraction: false,
+							}}
+							onSlideChange={(swiper) => setFocusedItem(swiper.activeIndex)}
+							modules={[Autoplay, Pagination]}
+							className="w-full sm:w-2/4"
+						>
+							{data.map((item, i) => (
+								<SwiperSlide key={i} className="self-center">
+									<CardHome
+										id={item.id}
+										name={item.name}
+										rating={item.rating}
+										img_urls={item.img_urls[0]}
+										focused={i === focusedItem}
+										isWishlishted={item.wishlist}
+									/>
+								</SwiperSlide>
+							))}
+						</Swiper>
+					</main>
+				</>
+			)}
 			
 		</Layout>
 	);
